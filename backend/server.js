b8af963cd4210c9103761cdb4ced88089ec0b840
Eleni Veniou Nikolidaki <venelenii@gmail.com>
2024-07-19 22:58:23 +0300

    fix issues

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors")
+const path = require('path')
 const db = require("./config/connection")
 const journalRouter = require("./routes/journalRoute")
 const userRouter = require("./routes/userRoute")
@@ -11,20 +12,23 @@ const PORT = process.env.PORT;
 const FRONTEND_URI = process.env.FRONTEND_URI
 
 app.use(express.json());
+
+app.use(express.static(path.join(__dirname, 'build')))
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
  
 app.use(cors({
-  origin: "*",
-  // origin: FRONTEND_URI, // allow your client app
-  // methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // allowed methods
-  // allowedHeaders: ['Content-Type', 'Authorization', 'x-auth-token'], // allowed headers
-  // credentials: true // if you need to send cookies or other crede
+  // origin: "*",
+  origin: FRONTEND_URI, // allow your client app
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // allowed methods
+  allowedHeaders: ['Content-Type', 'Authorization', 'x-auth-token'], // allowed headers
+  credentials: true // if you need to send cookies or other crede
   }));
   
-
 app.use("/journal", journalRouter)
 app.use("/user", userRouter)
-
-
 
 app.listen(PORT, ()=> {
     console.log(`Server is running on: http://localhost:${PORT}`)
