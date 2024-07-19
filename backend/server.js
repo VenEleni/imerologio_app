a917370369d68a4af516ae3e80f2ff20d07f36d5
Eleni Veniou Nikolidaki <venelenii@gmail.com>
2024-07-19 23:34:09 +0300

    fix issue no29

--- backend/server.js
+++ backend/server.js
@@ -11,6 +11,7 @@ const app = express();
 const PORT = process.env.PORT;
 const FRONTEND_URI = process.env.FRONTEND_URI
 
+
 app.use(express.json());
 
 app.use(express.static(path.join(__dirname, 'build')))
